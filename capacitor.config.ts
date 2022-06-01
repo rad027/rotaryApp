1299b85cf4a98ecfb6f00b4d8744d8220383c11e
Roldhan Dasalla <roldhan27@Roldhans-MacBook-Air.local>
2022-06-01 22:50:45 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'rotaryApp',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
